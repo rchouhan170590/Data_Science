b3f177d8c131c75c8d87d51e3cdb5d601cf25e51
ROHIT CHOUHAN <43743224+rchouhan170590@users.noreply.github.com>
2019-05-24 18:53:30 +0530

    Add files via upload

---
+++ sqlite3/create_table.py
@@ -0,0 +1,32 @@
+#create table called "classroom" in database "classroomDB.db"
+#On jupyter notebook 
+
+
+
+import sqlite3
+
+#open connection
+connection = sqlite3.connect("classroomDB.db")
+
+#open cursor
+cursor = connection.cursor()
+
+#query for creating table
+create_table = """
+                         CREATE TABLE classroom (
+                           student_id INTEGER PRIMARY KEY,
+                           name VARCHAR(20),
+                           gender VARCHAE(1),
+                           physics_marks INTEGER,
+                           chemistr_marks INTEGER,
+                           mathematics_marks INTEGER
+                          ); """
+
+#execute query
+cursor.execute(create_table)
+
+#commit change
+connection.commit()
+
+#close connection
+connection.close()
