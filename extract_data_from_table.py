1f6279bd30085baff47c58bc28df9db0a8631d1b
ROHIT CHOUHAN <43743224+rchouhan170590@users.noreply.github.com>
2019-05-24 18:54:04 +0530

    Delete extract_data_from_table.py

--- extract_data_from_table.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-connection = sqlite3.connect("classroomDB.db")
-cursor = connection.cursor()
-query = "SELECT  *  FROM classroom"
-cursor.execute(query)
-result = cursor.fetchall()
-for row in result :
-      print (row)
-connection.close()
